070c3234532d7a8108a689f98a0190138915d5aa
DongGyuJang <cdgu1129@knou.ac.kr>
2022-10-16 21:45:48 +0900

    feat: 메이플 캐릭터 랭크페이지 크롤링 기능 추가 (테스트 추가)

---
+++ src/main/java/com/nexon/maple/comm/maplestoryHomepage/object/MapleCharacter.java
@@ -0,0 +1,34 @@
+package com.nexon.maple.comm.maplestoryHomepage.object;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class MapleCharacter{
+    String image;
+    Long rank;
+    String rankMove;
+    String userName;
+    String job1;
+    String job2;
+    String level;
+    String experience;
+    Long popularity;
+    String guildName;
+
+    @Builder
+    public MapleCharacter(String image, Long rank, String rankMove, String userName, String job1, String job2, String level, String experience, Long popularity, String guildName) {
+        this.image = image;
+        this.rank = rank;
+        this.rankMove = rankMove;
+        this.userName = userName;
+        this.job1 = job1;
+        this.job2 = job2;
+        this.level = level;
+        this.experience = experience;
+        this.popularity = popularity;
+        this.guildName = guildName;
+    }
+}
