bf52b6558868487e3dc6060e57a4320213fb95c3
DongGyuJang <cdgu1129@knou.ac.kr>
2022-10-17 14:51:11 +0900

    refactor: async를 위한 공통함수 추출

--- src/main/resources/static/js/comm/fetch.js
+++ src/main/resources/static/js/comm/fetch.js
@@ -34,6 +34,30 @@ const mapleFetch = (url, method, headers, body, then) => {
         .finally(() => loading = false);
 }
 
+const mapleFetchAsync = async (url, method, headers, body) => {
+    if(loading) {
+        console.log("작업이 진행중입니다.");
+        return;
+    }
+
+    let fetchConfig = {};
+
+    if(url === null || method === null) {
+        return;
+    }
+    fetchConfig.method = method;
+
+    if(headers === null) {
+        fetchConfig.headers = headers;
+    }
+
+    if(body !== null) {
+        fetchConfig.body = body;
+    }
+
+    return await fetch(url, fetchConfig);
+}
+
 //form id를 입력받아 폼데이터를 json으로 반환
 const getFormDataJson = (id) => {
     const frmRegister = document.getElementById(id);
--- src/main/webapp/WEB-INF/views/user/regist.jsp
+++ src/main/webapp/WEB-INF/views/user/regist.jsp
@@ -19,9 +19,7 @@
                 <c:otherwise>(선택)</c:otherwise>
             </c:choose>
         </h5>
-        <textarea>
-            ${item.content}
-        </textarea>
+        <textarea>${item.content}</textarea>
         <div>
             <input type="checkbox" name="checkbox" code="${item.code}" types="${item.type}" required="${item.requiredTerms}">
             <span>동의합니다.</span>
