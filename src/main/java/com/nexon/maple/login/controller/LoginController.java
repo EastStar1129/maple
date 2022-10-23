b600bb0a48e4cda5fa9816a9989270885b2a714e
DongGyuJang <cdgu1129@knou.ac.kr>
2022-10-24 06:04:13 +0900

    refactor: controller 정리

--- src/main/java/com/nexon/maple/login/controller/LoginController.java
+++ src/main/java/com/nexon/maple/login/controller/LoginController.java
@@ -1,35 +1,46 @@
 package com.nexon.maple.login.controller;
 
 
+import com.nexon.maple.config.security.auth.PrincipalDetails;
 import com.nexon.maple.config.security.jwt.JwtToken;
-import com.nexon.maple.login.service.LoginService;
+import com.nexon.maple.userInfo.entity.UserInfo;
+import com.nexon.maple.userInfo.repository.UserInfoDao;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 public class LoginController {
-    private final LoginService loginService;
     private final JwtToken jwtToken;
+    private final UserInfoDao userInfoDao;
 
-    @GetMapping("/logout")
-    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @GetMapping("/effectiveToken")
+    public ResponseEntity effectiveToken(HttpServletResponse response) {
+        return ResponseEntity.status(response.getStatus()).build();
+    }
 
-        for(Cookie cookie: request.getCookies()) {
-            if(cookie.getName().equals(jwtToken.getHeader())) {
-                cookie.setMaxAge(0);
-                cookie.setPath("/"); // 모든 경로에서 삭제 됬음을 알린다.
-                response.addCookie(cookie);
-            }
+    @GetMapping("/reissue")
+    public ResponseEntity reissue(HttpServletResponse response, HttpServletRequest request) {
+        if(response.getHeader("x-token").equals("true")) {
+            String accessToken = jwtToken.typeRemove(request.getHeader(jwtToken.getHeader()));
+            String userName = jwtToken.getUserName(accessToken);
+
+            UserInfo userInfo = userInfoDao.findByName(userName);
+            response.addHeader(jwtToken.getHeader(),
+                    jwtToken.getType()+ " " + jwtToken.generateAccessToken(new PrincipalDetails(userInfo)));
+
+            Cookie RefreshTokenCookie = new Cookie(jwtToken.getHeader(), jwtToken.generateRefreshToken());
+            RefreshTokenCookie.setMaxAge(jwtToken.REFRESH_TOKEN_EXPIRE_TIME);
+            RefreshTokenCookie.setHttpOnly(true);
+            RefreshTokenCookie.setSecure(true);
+            response.addCookie(RefreshTokenCookie);
         }
-
-        loginService.logout();
-        response.sendRedirect("/");
+        return ResponseEntity.status(response.getStatus()).build();
     }
 }
